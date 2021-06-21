1c711f12454c9388f5b8ee1e6da4e7a46ae44e44
meij0hn <meijohnnura@gmail.com>
2021-06-21 11:01:21 +0700

    20210621
    First Deploy

---
+++ setupxx/inc/footer.inc.php
@@ -0,0 +1,12 @@
+
+            <div class="clear"></div>
+        </div> <!-- content -->
+    </div> <!-- wizard -->
+    <div id="footer" class="centered">Copyright &copy; <?php echo date('Y'); ?> <a target="_blank" href="https://osticket.com">osTicket.com</a></div>
+
+    <script type="text/javascript" src="../js/jquery-3.4.0.min.js?833262f"></script>
+    <script type="text/javascript" src="../js/jstz.min.js?833262f"></script>
+    <script type="text/javascript" src="js/setup.js?833262f"></script>
+    <script type="text/javascript" src="js/tips.js?833262f"></script>
+</body>
+</html>
